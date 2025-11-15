d654df11e7b0533e8295ec5be2ef73149b83ead6
Petr Nejman <p.nejman@gmail.com>
2025-11-15 10:31:23 +0100

    NuGet settings added

--- PN.ServiceScheduler/Interfaces/IJob.cs
+++ PN.ServiceScheduler/Interfaces/IJob.cs
@@ -5,8 +5,6 @@
         /// <summary>
         /// Executes the job asynchronously.
         /// </summary>
-        /// <param name="stoppingToken"></param>
-        /// <returns></returns>
         Task ExecuteAsync(CancellationToken stoppingToken);
     }
 }
